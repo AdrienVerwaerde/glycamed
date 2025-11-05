5fa80def687fab606f86bfd63f3b1dbd2ec60961
AdrienVerwaerde <verwaerdeadrien@gmail.com>
2025-11-05 11:50:23 +0100

    frontend-feat: layout to get header everywhere

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,25 +6,27 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProtectedRoute from "./components/Auth/ProtectedRoute";
 import AmedPage from "./pages/AmedPage";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
     <div className="App">
-      {/* Routes */}
       <main>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/amed"
+              element={
+                <ProtectedRoute>
+                  <AmedPage />
+                </ProtectedRoute>
+              }
+            />
 
-          <Route
-            path="/amed"
-            element={
-              <ProtectedRoute>
-                <AmedPage />
-              </ProtectedRoute>
-            }
-          />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Route>
         </Routes>
       </main>
     </div>
